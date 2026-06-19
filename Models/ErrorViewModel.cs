821adf12d754a68f08015e2a3c7603bbf1c9a39d
tram14012004 <tram14012004@gmail.com>
2026-06-19 22:47:07 +0700

    Create_project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _2526_2221050858_BaiThi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
