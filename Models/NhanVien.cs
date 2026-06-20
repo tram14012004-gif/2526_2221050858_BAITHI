05f263fae5222c6d42387dbda775de3a40ad15de
tram14012004 <tram14012004@gmail.com>
2026-06-20 15:51:56 +0700

    Cau4

---
+++ Models/NhanVien.cs
@@ -0,0 +1,7 @@
+namespace  _2526_2221050858_BaiThi.Models;
+  public class NhanVien
+  {
+    public string MaNhanVien { get; set; }
+    public string TenNhanVien  { get; set; }
+    public string MaPhongBan { get; set; } 
+  }
